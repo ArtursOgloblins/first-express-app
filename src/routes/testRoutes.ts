60edea13cc4ab3c3512ca40cfcc2cc62d74d5b34
SpringJack <artur0_g@proton.me>
2023-09-23 19:28:53 +0300

    uncomment delete videos and blogs

--- src/routes/testRoutes.ts
+++ src/routes/testRoutes.ts
@@ -4,8 +4,8 @@ import {testRepository} from "../repositories/testRepository";
 const testRouter = express.Router();
 
 testRouter.delete('/all-data', (req:Request, res:Response) => {
-    //testRepository.deleteAllVideos()
-    //testRepository.deleteAllBlogs()
+    testRepository.deleteAllVideos()
+    testRepository.deleteAllBlogs()
     res.status(204)
 })
 
