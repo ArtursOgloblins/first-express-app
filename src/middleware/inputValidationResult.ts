33cc9d9b75cde3d114d49633a0759ce06aec2b5e
SpringJack <artur0_g@proton.me>
2023-09-23 23:49:07 +0300

    validations updated

--- src/middleware/inputValidationResult.ts
+++ src/middleware/inputValidationResult.ts
@@ -2,10 +2,10 @@ import {NextFunction, Request, Response} from "express";
 import {validationResult} from "express-validator";
 
 export const InputValidationResult = (req:Request, res: Response, next: NextFunction) => {
-    const errors = validationResult(req)
-    if (errors.isEmpty()) {
+    const errorsMessages = validationResult(req)
+    if (errorsMessages.isEmpty()) {
         return  next()
     }
-    res.status(400).send({errors: errors.array({onlyFirstError: true})})
+    res.status(400).send({errors: errorsMessages.array({onlyFirstError: true})})
 
 }
