790f0ac88e4361f31046fcd7a6ec65563b7e9941
SpringJack <artur0_g@proton.me>
2023-09-23 13:54:44 +0300

    add getBlogById query

--- src/repositories/blogs/BlogsRepository.ts
+++ src/repositories/blogs/BlogsRepository.ts
@@ -3,5 +3,29 @@ import {db, Blog} from "../../models/Blogs";
 export const blogsRepository ={
     getBlogs(): Blog[] {
         return db.blogs
+    },
+
+    getBlogById (id: number): Blog {
+        return <Blog>db.blogs.find(b => b.id === id)
+    },
+
+    deleteBlogById(id: number): boolean{
+        for (let i = 0; i < db.blogs.length; i++){
+            if (db.blogs[i].id == id) {
+                db.blogs.splice(i, 1)
+                return true
+            }
+        }
+        return false
+
+        // removeBlogById(id: number): Blog | null {
+        //     const index = db.blogs.findIndex(blog => blog.id === id);
+        //     if (index !== -1) {
+        //         const removedBlog = db.blogs[index];
+        //         db.blogs = [...db.blogs.slice(0, index), ...db.blogs.slice(index + 1)];
+        //         return removedBlog;
+        //     }
+        //     return null;
+        // }
     }
 }
--- src/routes/blogsRoutes.ts
+++ src/routes/blogsRoutes.ts
@@ -1,11 +1,21 @@
 import express, {Request, Response} from "express";
 import {blogsRepository} from "../repositories/blogs/BlogsRepository";
+import {videosRepository} from "../repositories/videos/videosRepository";
 
 const blogRouter = express.Router();
 
 blogRouter.get('/', (req: Request, res: Response) => {
     const blogs = blogsRepository.getBlogs()
     res.send(blogs)
+})
+
+blogRouter.get('/:id', (req: Request, res: Response) => {
+    const blog = videosRepository.deleteVideoById(+req.params.id)
+    if (blog) {
+        res.send(blog)
+    } else {
+        res.sendStatus(404)
+    }
 })
 
 export default blogRouter
