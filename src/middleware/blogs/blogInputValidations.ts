12afdd2b3e9abcb849e6bbe793b1729b4a2e2ed1
SpringJack <artur0_g@proton.me>
2023-09-24 14:20:42 +0300

    update blogInput validations

--- src/middleware/blogs/blogInputValidations.ts
+++ src/middleware/blogs/blogInputValidations.ts
@@ -4,12 +4,14 @@ import {body} from "express-validator";
 export const blogValidationPost = [
     body('name')
         .exists().withMessage('Name field is required')
-        .trim()
+        .notEmpty().withMessage('Name field must not be empty')
+        .trim().withMessage('Wrong format')
         .isLength({ max: 10 })
         .withMessage('Max length 10 '),
     body('description')
         .exists().withMessage('Name field is required')
-        .trim()
+        .notEmpty().withMessage('Name field must not be empty')
+        .trim().withMessage('Wrong format')
         .isLength({max: 500}).withMessage('Max length 500'),
     body('websiteUrl')
         .exists().withMessage('Name field is required')
