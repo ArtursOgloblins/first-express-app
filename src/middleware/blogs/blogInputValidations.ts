f37979c66bb8928659412dedd329abd9c10a11b8
SpringJack <artur0_g@proton.me>
2023-09-24 14:27:22 +0300

    update blogInput validations

--- src/middleware/blogs/blogInputValidations.ts
+++ src/middleware/blogs/blogInputValidations.ts
@@ -3,20 +3,18 @@ import {body} from "express-validator";
 
 export const blogValidationPost = [
     body('name')
+        .trim()
         .exists().withMessage('Name field is required')
         .notEmpty().withMessage('Name field must not be empty')
-        .trim().withMessage('Wrong format')
-        .isLength({ max: 10 })
-        .withMessage('Max length 10 '),
+        .isLength({ max: 10 }).withMessage('Max length 10'),
     body('description')
+        .trim().withMessage('Wrong format')
         .exists().withMessage('Name field is required')
         .notEmpty().withMessage('Name field must not be empty')
-        .trim().withMessage('Wrong format')
         .isLength({max: 500}).withMessage('Max length 500'),
     body('websiteUrl')
-        .exists().withMessage('Name field is required')
         .trim()
-        .isLength({max: 100})
-        .matches(/^https:\/\/([a-zA-Z0-9_-]+\.)+[a-zA-Z0-9_-]+(\/[a-zA-Z0-9_-]+)*\/?$/)
-        .withMessage('Invalid URL format')
+        .exists().withMessage('Name field is required')
+        .isLength({max: 100}).withMessage('Max length 100')
+        .matches(/^https:\/\/([a-zA-Z0-9_-]+\.)+[a-zA-Z0-9_-]+(\/[a-zA-Z0-9_-]+)*\/?$/).withMessage('Invalid URL format')
 ]
