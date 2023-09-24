9020eb2fcbbe2fc07cc256f5a5eb67eea260c91b
SpringJack <artur0_g@proton.me>
2023-09-24 13:33:29 +0300

    update validation results

--- src/middleware/inputValidationResult.ts
+++ src/middleware/inputValidationResult.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from "express";
-import { validationResult } from "express-validator";
+import { validationResult, ValidationError  } from "express-validator";
+
 
 export const InputValidationResult = (req: Request, res: Response, next: NextFunction) => {
     const errors = validationResult(req);
@@ -7,14 +8,13 @@ export const InputValidationResult = (req: Request, res: Response, next: NextFun
         return next();
     }
 
-    // Map through the errors array and restructure each error object
-    const errorsMessages = errors.array({ onlyFirstError: true }).map(error => {
+    const errorsMessages = errors.array({ onlyFirstError: true })
+        .map((error: ValidationError & { path?: string }) => {
         return {
             message: error.msg,
-            field: (error as any).param,
+            field: error.path,
         };
     });
 
-    // Send the response with the restructured error objects
     res.status(400).send({ errorsMessages });
 }
