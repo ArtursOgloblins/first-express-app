8ac58913af430e93c03cdd6b899cbed41317706b
SpringJack <artur0_g@proton.me>
2023-09-30 23:59:15 +0300

    add mongo url

--- src/repositories/db.ts
+++ src/repositories/db.ts
@@ -3,7 +3,7 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 const mongoUri = process.env.MONGO_URL || "mongodb://0.0.0.0:27017/?maxPoolSize=20&w=majority"
-const dbName = process.env.DB_NAME || "blogs_posts";
+//const dbName = process.env.DB_NAME || "blogs_posts";
 
 export const client = new MongoClient(mongoUri)
 
